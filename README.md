41d244f39672cff5967757521eaef79f92073bd3
Beau Winiger <bcwiniger@gmail.com>
2018-02-22 23:30:20 -0500

    finished out email; cleaned up message

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ This script relies on the presence of an .env file in the working directory.
 It has the below values
 
 `STRAVA_TOKEN="your-token"`
+
 `EMAIL_TO="to@example.com"`
+
 `EMAIL_FROM="from@example.com"`
+
 `EMAIL_PWD="password"`
--- push.py
+++ push.py
@@ -4,22 +4,29 @@ from datetime import datetime
 from datetime import timedelta
 import smtplib
 from stravalib import unithelper
+from email.mime.text import MIMEText
+import calendar
 
 def main():
     now = datetime.utcnow()
-    start = end_of_month(now)
+    start = get_range_start(now)
     end = datetime(now.year, now.month, 1, 0, 0, 1)
     
     dotenv.load()
     total_miles = get_total_miles(start, end, dotenv.get("STRAVA_TOKEN"))
 
-    send_email(total_miles, dotenv.get("EMAIL_TO"), dotenv.get("EMAIL_FROM"), dotenv.get("EMAIL_PWD"))
+    message = "Total Mileage for month of {0}: {1} miles".format(calendar.month_name[get_prev_month(now)], total_miles)
 
-def end_of_month(now):
-    prev_month = (now.replace(day=1) - timedelta(days=1)).month
+    send_email(message, dotenv.get("EMAIL_TO"), dotenv.get("EMAIL_FROM"), dotenv.get("EMAIL_PWD"))
+
+def get_range_start(now):
+    prev_month = get_prev_month(now)
     first_of_prev_month = now.replace(day=1).replace(month=prev_month)
     end = first_of_prev_month - timedelta(days=1)
     return datetime(end.year, end.month, end.day, 23, 59, 59)
+
+def get_prev_month(now):
+    return (now.replace(day=1) - timedelta(days=1)).month
 
 def get_total_miles(start, end, token):
     dotenv.load()
@@ -31,13 +38,17 @@ def get_total_miles(start, end, token):
     
     return int(total_miles)
 
-def send_email(total_miles, email_to, email_from, email_pwd):
+def send_email(msg, email_to, email_from, email_pwd):
+    email = MIMEText(msg)
+    email['Subject'] = "Monthly Mileage"
+    email['From'] = email_from
+    email['To'] = email_to
+
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(email_from, email_pwd)
 
-    msg = "Monthly Mileage: " + str(total_miles)
-    server.sendmail(email_from, email_to, msg)
+    server.sendmail(email_from, email_to, email.as_string())
     server.quit()
 
 main()
