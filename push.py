31e8efd4e608499ce7786cc9c5599e3c410a0792
Beau Winiger <bcwiniger@gmail.com>
2018-02-22 22:45:49 -0500

    switched to unithelper to get miles

--- push.py
+++ push.py
@@ -3,6 +3,7 @@ from stravalib.client import Client
 from datetime import datetime
 from datetime import timedelta
 import smtplib
+from stravalib import unithelper
 
 def main():
     now = datetime.utcnow()
@@ -15,9 +16,9 @@ def main():
     send_email(total_miles, dotenv.get("EMAIL_TO"), dotenv.get("EMAIL_FROM"), dotenv.get("EMAIL_PWD"))
 
 def end_of_month(now):
-    prevMonth = (now.replace(day=1) - timedelta(days=1)).month
-    firstOfPrevMonth = now.replace(day=1).replace(month=prevMonth)
-    end = firstOfPrevMonth - timedelta(days=1)
+    prev_month = (now.replace(day=1) - timedelta(days=1)).month
+    first_of_prev_month = now.replace(day=1).replace(month=prev_month)
+    end = first_of_prev_month - timedelta(days=1)
     return datetime(end.year, end.month, end.day, 23, 59, 59)
 
 def get_total_miles(start, end, token):
@@ -26,7 +27,7 @@ def get_total_miles(start, end, token):
 
     total_miles = 0
     for activity in client.get_activities(after=start, before=end):
-        total_miles += (activity.distance.num * 0.000621371)
+        total_miles += unithelper.miles(activity.distance).num
     
     return int(total_miles)
 
